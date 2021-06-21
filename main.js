58e650e3915401e235595ab8554fb86ec3451b71
SamratWhiteHatJr <86223057+SamratWhiteHatJr@users.noreply.github.com>
2021-06-21 16:03:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function li_1_click(){
+    window.location="li-1.html";
+}
+
+function back_to_menu(){
+    window.location="index.html";
+}
+
+function li_2_click(){
+    window.location="li-2.html";
+}
+
+function  li_3_click(){
+    window.location="li-3.html";
+}
